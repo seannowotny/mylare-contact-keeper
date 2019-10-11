ac39bb8014f5f971de2423cd4838dd46db5f911a
seannowotny <seannowotny@live.de>
2019-10-11 10:47:51 +0200

    Initial routing

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Contact;
+use App\Http\Resource\ContactCollection;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function()
+{
+    return 'Hello';
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/contacts', function()
+{
+    return new ContactCollection(Contact::all());
 });
