b0ed52ddced64443a52f96e6241fabca7fbbc04f
seannowotny <seannowotny@live.de>
2019-10-11 10:52:52 +0200

    Configure fileds for contacts table

--- database/migrations/2019_10_11_075513_create_contacts_table.php
+++ database/migrations/2019_10_11_075513_create_contacts_table.php
@@ -15,6 +15,10 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('type');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
