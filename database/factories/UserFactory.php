85746ea0e521e58502d9ec59e9ee147a69445663
seannowotny <seannowotny@live.de>
2019-10-11 11:42:01 +0200

    Create UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,15 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$0WYN.TWI67yPHeTs5ocAM.VsPwdVQ.BFpFAAWJ2IGIkVFlyswli8K',
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->state(User::class, 'jd', function() {
+    return [
+        'name' => 'John Doe',
+        'email' => 'jdoe@gmail.com',
+        'password' => '$2y$10$0WYN.TWI67yPHeTs5ocAM.VsPwdVQ.BFpFAAWJ2IGIkVFlyswli8K',
     ];
 });
